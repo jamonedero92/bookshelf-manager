1324f962c632ea3ab7ff713d37e680150971cc80
Jorge <jamonedero92ggmail.com>
2025-08-23 14:21:02 -0700

    add: BookshelfController.java as controller for the CRUD operations with the books.

---
+++ src/main/java/com/github/jamonedero92/bookshelf_manager/controllers/BookshelfController.java
@@ -0,0 +1,13 @@
+package com.github.jamonedero92.bookshelf_manager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookshelfController {
+
+    @GetMapping("/")
+    public String showHome(){
+        return "home";
+    }
+}
