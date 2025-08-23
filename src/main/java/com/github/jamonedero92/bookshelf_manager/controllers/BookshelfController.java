b3989e367a76a28a5ae29acb53ed211cba750b07
Jorge <jamonedero92ggmail.com>
2025-08-23 14:57:53 -0700

    feat: add the method showBooksList to the BookshelfController, used to load the books-list.html.

--- src/main/java/com/github/jamonedero92/bookshelf_manager/controllers/BookshelfController.java
+++ src/main/java/com/github/jamonedero92/bookshelf_manager/controllers/BookshelfController.java
@@ -1,6 +1,7 @@
 package com.github.jamonedero92.bookshelf_manager.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +10,11 @@ public class BookshelfController {
     @GetMapping("/")
     public String showHome(){
         return "home";
+    }
+
+    @GetMapping("/books-list")
+    public String showBooksList(ModelMap model){
+
+        return "books-list";
     }
 }
